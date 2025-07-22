233009ded6022a59b7be17113c5e0a980309c306
Damanjeet Singh <damanjeetsingh434@gmail.com>
2025-07-22 21:54:02 +0530

    removed env loading
    Signed-off-by: Damanjeet Singh <damanjeetsingh434@gmail.com>

--- server/pkg/config/app.go
+++ server/pkg/config/app.go
@@ -4,18 +4,16 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/redis/go-redis/v9"
 )
 
 var redisClient *redis.Client
 
 func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("No .env variables found")
-	}
-
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Println("No .env variables found")
+	// }
 
 	log.Println("Connecting to Redis...")
     redisClient= redis.NewClient(&redis.Options{
